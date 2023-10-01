f479595234bde14722b08e895716e2b2d128678d
Tegardf <zoel.tegar234@gmail.com>
2023-10-01 15:19:12 +0700

    -adding logic to javascript -logic for calculate value of BMI -logic for choose type of BMI -logic for pop up container -logic for warning if input empty

---
+++ script.js
@@ -0,0 +1,42 @@
+const calBMI = (berat,tinggi) => {
+    if (!isNaN(berat)&&!isNaN(tinggi)) {
+        return berat/((tinggi/100)**2);   
+    }
+}
+
+const checkBMI = (BMI) =>{
+    if (BMI <= 18.5) return 'Underweight';
+    else if (BMI>18.5 && BMI<25) return 'Normal Weight';
+    else if (BMI>=25 && BMI<30) return 'Overweight';
+    else if (BMI>=30) return 'Obesity';
+}   
+
+const togglePopup = () =>{
+    let popUp = document.getElementById("floating-result");
+    popUp.style.display = (popUp.style.display === "block") ? "none" : "block";
+}
+
+const submit = (event) =>{
+    event.preventDefault();
+    berat = document.getElementById("berat").value;
+    tinggi = document.getElementById("tinggi").value;
+    container = document.getElementById('warning');
+
+    if(berat!='' && tinggi !=''){
+        container.replaceChildren();
+        togglePopup();
+        result = calBMI(berat,tinggi);
+        document.getElementById("BMI-value").innerHTML = result;
+        document.getElementById("BMI-cat").innerHTML = checkBMI(result);
+    }else if (berat=='' && tinggi=='') {
+        container.innerHTML = '<p>Silahkan masukkan Berat dan Tinggi anda</p>';
+    }else if (tinggi=='') {
+        container.innerHTML = '<p>Silahkan masukkan Tinggi anda</p>';
+    }else {
+        container.innerHTML = '<p>Silahkan masukkan Berat anda</p>';
+    }
+}
+
+document.getElementById("submit-form").addEventListener("click",submit);
+document.getElementById("close").addEventListener("click",togglePopup);
+
